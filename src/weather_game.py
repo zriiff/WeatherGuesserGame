a43e8834112404fd1727e5218fecb0c9299c3487
zriiff <zriiff@gmail.com>
2024-03-15 00:00:18 -0400

    added play_game() function

--- src/weather_game.py
+++ src/weather_game.py
@@ -1,6 +1,17 @@
+import random
 import requests
+import csv
 
 API_KEY = "5c26a30700394a02465c639499c61e42"
+
+
+def get_places():
+    places = []
+    with open("src/places.csv", mode='r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            places.append(f"{row['city']}, {row['country']}")
+    return places
 
 
 def get_weather_temp(place):
@@ -15,11 +26,38 @@ def get_weather_temp(place):
     return weather, tempurature
 
 
+def play_game(c: list):
+    score = 0
+    while True:
+        cities = c
+        city1 = cities[random.randint(0, len(cities) - 1)]
+        cities.remove(city1)
+        city2 = cities[random.randint(0, len(cities) - 1)]
+        weather1, temp1 = get_weather_temp(city1)
+        weather2, temp2 = get_weather_temp(city2)
+        print(f"{city1} vs {city2}")
+        num = int(input("Which place has a higher temp (1,2): "))
+        if num == 1:
+            if temp1 > temp2:
+                print(f"You win! ({temp1}°F > {temp2}°F)\n")
+                score += 1
+            else:
+                print(f"You lose ({temp1}°F > {temp2}°F)\n")
+                break
+        else:
+            if temp2 > temp1:
+                print(f"You win! ({temp1}°F < {temp2}°F)\n")
+                score += 1
+            else:
+                print(f"You lose ({temp1}°F < {temp2}°F)\n")
+                break
+    return score
+
+
 def main():
-    andover = "Andover, Massachusetts, US"
-    place = "Torraca, Italy"
-    andover_weather, andover_temp = get_weather_temp(place)
-    print(f"In {place}, it is currently \"{andover_weather}\" with a temperature of {andover_temp}°F")
+    cities = get_places()
+    score = play_game(cities)
+    print(f"Score: {score}")
 
 
 if __name__ == '__main__':
